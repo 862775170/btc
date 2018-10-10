a477bd93af2a0b3e9636fd2e4f181feadbcf0a5c
chen9 <862775170@qq.com>
2018-10-10 22:09:11 +0800

    update .gitignore

--- btc-common/src/main/java/com/btc/common/util/UUIDUtil.java
+++
@@ -1,9 +0,0 @@
-package com.btc.common.util;
-
-import java.util.UUID;
-
-public class UUIDUtil {
-	public static String generateUUID() {
-		return UUID.randomUUID().toString().replace("-", "").toUpperCase();
-	}
-}
